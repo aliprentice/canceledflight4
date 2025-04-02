dfe1cc92a9780b46a41502fc979dc4e4bcec1969
aliprentice <57479066+aliprentice@users.noreply.github.com>
2025-04-02 18:06:14 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  webpack: (config, { isServer }) => {
+    // If client-side, don't polyfill Node.js modules
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        dns: false,
+        child_process: false,
+        http2: false,
+        path: false,
+        os: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
